ef634c9c55e0e50ca04027d8cbb9c1fe094efdeb
KurtMoucketou <kurtmoucketou@yahoo.fr>
2021-02-09 12:40:57 +0100

    StartHamb

--- pages/teste.js
+++ pages/teste.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import Titre from './components/Titre';
 import SousTitre from './components/SousTitre';
+import { Row, Col } from 'reactstrap';
+import Menu from './components/MenuHamb';
 
 
 export default function teste() {
     return (
         <div>
-            <SousTitre color='#000000' texte='Mon titre' />
-            <Titre color='#000000' texte='Mon titre' />
+            <Row>
+                <Col>
+                    <SousTitre color='#000000' texte='Mon titre' />
+                    <Titre color='#000000' texte='Mon titre' />
+                </Col>
+
+                <Col>
+                </Col>
+            </Row>
         </div>
 
     )
