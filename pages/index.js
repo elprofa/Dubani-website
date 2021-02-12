bbe31e0191867389c8db4705edc60ace8099bdae
elprofa <wedoprofan14@gmail.com>
2021-02-12 04:06:04 +0100

    commit 2 dev1

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,26 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-
-import SectionAccueilMoleculeLeft from '../components/molecule/SectionAccueilMoleculeLeft';
+import SectionAccueil from '../components/template/SectionAccueil';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className="App">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-       <SectionAccueilMoleculeLeft />
+      <main className="main">
+        <SectionAccueil/>
       </main>
 
-      <footer className={styles.footer}>
+      <footer className="footer">
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
           Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
         </a>
       </footer>
     </div>
