5731c5f34cd236f903d48b61a9a59cccec6d25b6
elprofa <wedoprofan14@gmail.com>
2021-02-23 16:05:31 +0100

    correct some mistakes

---
+++ components/template/SectionContact/SectionContact.stc.js
@@ -0,0 +1,10 @@
+import Styled from 'styled-components';
+
+const SectionContactStc=Styled.section`
+    min-height:100vh;
+    padding-top:10%;
+
+    
+    margin:40px 0px;
+`;
+export default SectionContactStc;
