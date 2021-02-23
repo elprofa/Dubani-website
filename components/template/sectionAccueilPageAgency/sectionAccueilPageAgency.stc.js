1eb952355182e260a2ac21ac0f33df5e137ec23c
elprofa <wedoprofan14@gmail.com>
2021-02-23 17:36:53 +0100

    last page

--- components/template/sectionAccueilPageAgency/sectionAccueilPageAgency.stc.js
+++ components/template/sectionAccueilPageAgency/sectionAccueilPageAgency.stc.js
@@ -5,5 +5,13 @@ const sectionAccueilPageAgencyStc=Styled.section`
     background-size:cover;
     height:100vh;
     padding-top:15%;
+    @media (max-width: 767px)         
+    {
+        padding-top:15%;
+    }
+    @media (max-width: 476px)         
+    {
+        padding-top:30%;
+    }
 `;
 export default sectionAccueilPageAgencyStc;
